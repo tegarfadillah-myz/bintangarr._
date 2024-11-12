51a8f27b3f698ad96bd1a187abf064a430d2317c
tegarfadillah-myz <tegarfadila2233@gmail.com>
2024-11-12 14:10:17 +0700

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,32 @@
+/*=============== EMAIL JS ===============*/
+
+
+/*=============== SHOW SCROLL UP ===============*/ 
+
+const scrollUp = () => {
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll') : scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelector('section[id]')
+
+const scrollActive = () =>{
+    const  scrollDown = window.scrollY
+
+    section.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id'),
+            sectionClass = document.querySelector('.nav__list a[href*=' + sectionId +']')
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+
